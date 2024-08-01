800dce517afc405acf58896b2b20f73fb3b632da
Toby Schneider <toby@gobysoft.org>
2024-08-01 17:03:08 -0400

    Working example

--- demo/3_hash/demo.py
+++ demo/3_hash/demo.py
@@ -1,10 +1,43 @@
-import os, dccl, command_pb2
+#!/usr/bin/env python3
+import os, dccl
+import original_command_with_hash_pb2
+import updated_command_with_hash_pb2
 
-dccl.loadProtoFile(os.path.abspath("./command.proto"))
+def encode_decode_demo(codec, codec_updated, command):
+    
+    updated = updated_command_with_hash_pb2.UpdatedCommandWithHash()
+
+    print(f"- Original: **{str(command)[:-1]}**")
+    encoded = codec.encode(command)
+    
+    print("- Encoded (hex)")
+    print(f"  - {encoded.hex()} ({len(encoded)} bytes)")
+
+    decoded = codec.decode(encoded)
+
+    print("- Decoded:")
+    print(f"  - Using original message: {decoded}")
+
+    try:
+        decoded_updated = codec_updated.decode(encoded)
+    except dccl.DcclException as e:
+        print (f"  - Using updated message: Exception: {e}")
+
+
+
+dccl.loadProtoFile(os.path.abspath("./original_command_with_hash.proto"))
+dccl.loadProtoFile(os.path.abspath("./updated_command_with_hash.proto"))
 
 codec = dccl.Codec()
 codec.load("moos.dawg.OriginalCommandWithHash")
+codec.set_strict(True)
 
-r_out = command_pb2.OriginalCommandWithHash(action=command_pb2.OriginalCommandWithHash.MEASURE_THERMOCLINE, thermocline = {temperature_threshold: 1.2})
+# Receiver with updated message
+codec_updated = dccl.Codec()
+codec_updated.load("moos.dawg.UpdatedCommandWithHash")
+codec_updated.set_strict(True)
 
-print(r_out)
+command = original_command_with_hash_pb2.OriginalCommandWithHash(action=original_command_with_hash_pb2.OriginalCommandWithHash.MEASURE_THERMOCLINE, thermocline = {"max_search_depth": 100}, hash=0)
+
+print("# Hash via Message field (Runtime check)")
+encode_decode_demo(codec, codec_updated, command)
--- demo/3_hash/demo.sh
+++ demo/3_hash/demo.sh
@@ -6,17 +6,40 @@ function user_wait()
     echo
 }
 
-protoc --python_out=. command.proto 
+protoc --python_out=. original_command_with_hash.proto
+protoc --python_out=. updated_command_with_hash.proto
 
-(echo "# Hash via CLI"
+
+(echo "# Updated command (diff)"
 echo ''
-echo "- Original Hash: "
-dccl --analyze --proto_file ../1_oneof/command.proto -m moos.dawg.Command -H
-echo "- Updated Hash: "
-dccl --analyze --proto_file command.proto -m moos.dawg.Command -H
+echo '```'
+diff -u command.proto updated_command.proto
+echo '```') | md.py
 
+user_wait
+
+(echo "# Updated command with hash (diff)"
+echo ''
+echo '```'
+diff -u updated_command.proto updated_command_with_hash.proto
+echo '```') | md.py
+
+user_wait
+
+
+(echo "# Hash via CLI (Static analysis)"
+ echo ''
+ echo "- Original Hash: "
+ dccl --analyze --proto_file command.proto -m moos.dawg.Command -H
+ echo "- Updated Hash: "
+ dccl --analyze --proto_file updated_command.proto -m moos.dawg.Command -H
+ echo ""
+ echo "When possible you can compare these at compile time to detect unexpected backwards compatibility problems"
 ) | md.py
 
-#user_wait
+user_wait
+
+./demo.py |& ./md.py
+
 
 
